2fe7cf9af8be215d0bacd320369a1208e5279df5
Tu Nombre <issacgar98@gmail.com>
2025-01-30 01:05:12 -0600

    Integracion de Seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,13 +15,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
-
         \App\Models\User::create([
             'name' => 'Test User1',
             'email' => 'issacgar98@gmail.com',
