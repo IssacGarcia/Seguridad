89f887d3252e37e0a61fdfc0c922d7bae1a2f4c3
IssacGarcia <issacgar98@gmail.com>
2025-03-30 23:20:36 -0600

    se deshizo todo lo de las sesiones de los ultimos dos commits

--- config/session.php
+++ config/session.php
@@ -128,7 +128,7 @@ return [
 
     'cookie' => env(
         'SESSION_COOKIE',
-        env('COOKIE_PREFIX', Str::slug(env('APP_NAME', 'laravel'), '_').'_session')
+        Str::slug(env('APP_NAME', 'laravel'), '_').'_session'
     ),
 
     /*
